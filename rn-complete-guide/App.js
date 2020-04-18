6768b8e63be0a0067b333aee25f557e3505095e9
afischer <andreas.fischer@prodyna.com>
2020-04-19 00:20:37 +0200

    Initial Styles & Components

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder={"useless"} style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
+        <Button title={"ADD"} />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
